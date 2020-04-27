8f3cc5785654061aec9f03febdc2cd0eb4905844
morpheus <mariageorgepapas@gmail.com>
2020-04-27 01:45:03 -0500

    Integer equality printer & sleeping cat exercises

---
+++ PlayingCat/src/PlayingCat.java
@@ -0,0 +1,12 @@
+public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if(summer) {
+            if(temperature>=25 && temperature<=45) {
+                return true;
+            } return false;
+        } if(temperature>=25 && temperature<=35) {
+            return true;
+        } return false;
+    }
+}
