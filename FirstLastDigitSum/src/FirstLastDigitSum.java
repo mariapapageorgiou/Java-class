22b2856b8e8812c3be41a64d95c9374bf18511e0
morpheus <mariageorgepapas@gmail.com>
2020-09-15 20:57:51 -0500

    First and Last Digit Sum instructions

---
+++ FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -0,0 +1,8 @@
+public class FirstLastDigitSum {
+
+    // Write a method named sumFirstAndLastDigit with one parameter of type int called number.
+    // The method needs to find the first and the last digit of the parameter number passed to the method, using a loop
+    // and return the sum of the first and the last digit of that number.
+    // If the number is negative then the method needs to return -1 to indicate an invalid value.
+
+}
