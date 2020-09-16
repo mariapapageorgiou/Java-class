bd1cdfe9a310bdb71a3d437a17c8b44dcea5bb33
morpheus <mariageorgepapas@gmail.com>
2020-09-15 22:10:33 -0500

    Greatest Common Divisor instructions

---
+++ GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -0,0 +1,11 @@
+public class GreatestCommonDivisor {
+
+    // Write a method named getGreatestCommonDivisor with two parameters of type int named first and second.
+    // If one of the parameters is <10, the method should return -1 to indicate an invalid value. The method should
+    // return the greatest common divisor of the two numbers (int). The greatest common divisor is the largest positive
+    // integer that can fully divide each of the integers (i.e. without leaving a remainder).
+    // TIPS
+    // 1. Use a while or a for loop and check if both numbers can be divided without a remainder.
+    // 2. Find the minimum of the two numbers.
+
+}
