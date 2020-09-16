10abfb0ea38d72bf93ec2350f8001d3b064ceca2
morpheus <mariageorgepapas@gmail.com>
2020-09-15 21:09:18 -0500

    Even Digit Sum instructions

---
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -0,0 +1,7 @@
+public class EvenDigitSum {
+
+    // Write a method named getEvenDigitSum with one parameter of type int called number.
+    // The method should return the sum of the even digits within a number.
+    // If the number is negative, the method should return -1 to indicate an invalid value.
+
+}
