081a7cea7354355bb96a8da8a6fd6fda4429dad6
morpheus <mariageorgepapas@gmail.com>
2020-09-15 21:41:32 -0500

    Shared Digit solution

--- SharedDigit/src/SharedDigit.java
+++ SharedDigit/src/SharedDigit.java
@@ -5,4 +5,20 @@ public class SharedDigit {
     // range, the method should return false. The method should return true if there is a digit that appears in both
     // numbers, such as 2 in 12 and 23; otherwise, the method should return false.
 
+    public static boolean hasSharedDigit(int firstNumber, int secondNumber) {
+
+        if((firstNumber>=10) && (firstNumber<=99) && (secondNumber>=10) && (secondNumber<=99)){
+
+            int firstDigitFirstNum = firstNumber / 10;
+            int lastDigitFirstNum = firstNumber % 10;
+            int firstDigitSecondNum = secondNumber / 10;
+            int lastDigitSecondNum = secondNumber % 10;
+
+            if((firstDigitFirstNum==firstDigitSecondNum) || (firstDigitFirstNum == lastDigitSecondNum)
+                    || (lastDigitFirstNum == firstDigitSecondNum) || (lastDigitFirstNum == lastDigitSecondNum)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
