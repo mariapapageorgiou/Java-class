24c8107c22e25763a3c49019d7e5e7f2d36348d0
morpheus <mariageorgepapas@gmail.com>
2020-09-15 22:05:08 -0500

    Last Digit Checker solution

--- LastDigitChecker/src/LastDigitChecker.java
+++ LastDigitChecker/src/LastDigitChecker.java
@@ -8,4 +8,26 @@ public class LastDigitChecker {
     // Write another method named isValid with one parameter of type int.
     // The method needs to return true if the number is in range of 10(inclusive) - 1000(inclusive), otherwise return false.
 
+    public static boolean hasSameLastDigit(int numOne, int numTwo, int numThree) {
+
+        if(isValid(numOne) && isValid(numTwo) && isValid(numThree)) {
+
+            int lastNumOne = numOne % 10;
+            int lastNumTwo = numTwo % 10;
+            int lastNumThree = numThree % 10;
+
+            if((lastNumOne==lastNumTwo) || (lastNumOne==lastNumThree) || (lastNumTwo==lastNumThree)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isValid(int number) {
+
+        if(number>=10 && number<=1000) {
+            return true;
+        }
+        return false;
+    }
 }
