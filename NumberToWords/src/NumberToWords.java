c7cf91201807256093f08ed96f9e35c9b7f3ebc9
morpheus <mariageorgepapas@gmail.com>
2020-09-16 22:19:20 -0500

    Number to Words instructions (3 methods)

--- NumberToWords/src/NumberToWords.java
+++ NumberToWords/src/NumberToWords.java
@@ -1,6 +1,22 @@
 public class NumberToWords {
 
-    // Write a method called numberToWords with one int parameter named number.
-    // The method should print out the passed number using words for the digits.
-    // If the number is negative, print "Invalid Value".
+    // Write a method called numberToWords with one int parameter named number. The method should print out the passed
+    // number using words for the digits. If the number is negative, print "Invalid Value".
+    // To print the number as words, follow these steps:
+    // 1. Extract the last digit of the given number using the remainder operator.
+    // 2. Convert the value of the digit found into a word. There are 10 possible values for that digit, those being
+    //    0,1,2,3,4,5,6,7,8,9. Print the corresponding word for each digit.
+    // 3. Remove the last digit from the number.
+    // 4. Repeat steps 2 through 4 until the number is 0.
+
+    // Write a second method called reverse. The method should have one int parameter and return the reversed number(int).
+    // The method reverse should also reverse negative numbers.
+    // Use the method reverse within the method numberToWords in order to print the words in the correct order.
+
+    // Write a third method called getDigitCount. The method should have one int parameter called number and return the
+    // count of the digits in that number. If the number is negative, return -1 to indicate an invalid value.
+
+    // HINT: Use a for loop to print zeroes after reversing the number. To get the number of zeroes, check the difference
+    //       between the digit count from the original number and the reversed number.
+
 }
